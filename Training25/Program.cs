47bf69164f19fde0994d4dfd4b203dcb56d126ac
Eric <Eric.Sureshkumar@trumpf.com>
2025-09-24 15:57:17 +0530

    R02

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -8,23 +8,29 @@
 namespace Training25;
 
 internal class Program {
-   static string binaryConverter (int input) {
+   /// <summary>It is used to convert a normal decimal number to Binary number</summary>
+   /// <param name="input">Input number is taken from the user,takes only natural numbers</param>
+   /// <returns>Binary number for the given decimal number</returns>
+   static string BinaryConverter (int input) {
       string result = "";
       while (input != 0) {
-         int rem = input % 2;
+         int remainder = input % 2;
          input /= 2;
-         result = rem + result;
+         result = remainder + result;
       }
       return result;
    }
 
-   static string hexConverter (int input) {
-      string hexchar = "0123456789ABCDEF";
+   /// <summary>It is used to convert a normal decimal number to Binary number</summary>
+   /// <param name="input">Input number is taken from the user,takes only natural numbers</param>
+   /// <returns>Hexadecimal number for the given decimal number</returns>
+   static string HexConverter (int input) {
+      string hexChar = "0123456789ABCDEF";
       string result = "";
       while (input != 0) {
          int remainder = input % 16;
          input /= 16;
-         result = hexchar[remainder] + result;
+         result = hexChar[remainder] + result;
       }
       return result;
    }
@@ -32,12 +38,9 @@ internal class Program {
    static void Main (string[] args) {
       Console.WriteLine ("Enter a number");
       string input = Console.ReadLine ();
-      bool s = int.TryParse (input, out int result);
-      if (s) {
-         Console.WriteLine ($"Binary number:{binaryConverter (result)}");
-         Console.WriteLine ($"Hex number:{hexConverter (result)}");
-      } else {
-         Console.WriteLine ("Invalid input");
-      }
+      if (int.TryParse (input, out int result) && result != 0) {
+         Console.WriteLine ($"Binary number:{BinaryConverter (result)}");
+         Console.WriteLine ($"Hex number:{HexConverter (result)}");
+      } else Console.WriteLine ("Invalid input");
    }
 }
