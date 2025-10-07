51028c64908c24bc1b01c62efef4bbeb994946b0
Eric <Eric.Sureshkumar@trumpf.com>
2025-10-07 10:46:37 +0530

    R02

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,213 +3,122 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on branch T02.//This code is used to convert a number to words and roman letters.with a range check.
+// Program on branch T02 //This code is used to convert numbers to its equivalent words and Romanletters.
 // ------------------------------------------------------------------------------------------------
-using System.ComponentModel.Design;
-using System.Diagnostics.CodeAnalysis;
-
+using static System.Console;
 namespace Training25;
+
 internal class Program {
-   public static string numbertowords (int input) {
-      string[] ones = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
-                             "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
+   /// <summary>convert nuber to words upto 9999999</summary>
+   /// <param name="input">Takes whole number as input </param>
+   /// <returns>words equivalent to the number entered</returns>
+   static string NumberToWords (int input) {
+      string[] ones = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+                             "Nine","Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
                              "Sixteen", "Seventeen", "Eighteen", "Nineteen", };
-
-
-      string[] tens = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-
-
-      string Tens (int input) {
-         if (input > 19 && input < 100) {
-            int firstdigit = input / 10;
-            int seconddigit = input % 10;
-            return tens[firstdigit] + " " + ones[seconddigit];
-         } else if (input < 20) {
-            return ones[input];
-         } else {
-            return "";
-         }
+      string[] tens = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy",
+                                 "Eighty", "Ninety" };
+      String result = "";
+      if (input == 0) return "Zero";
+      if (input / 100000 > 0) {
+         result += NumberToWords (input / 100000) + "Lakh ";
+         input %= 100000;
       }
-      string[] hundreds = { "", "One Hundred", "Two Hundred", "Three Hundred", "Four Hundred", "Five Hundred", "Six Hundred", "Seven Hundred", "Eight Hundred", "Nine Hundred" };
-
-      string Hundreds (int input) {
-         int firstdigit = input / 100;
-         int seconddigit = input % 100;
-         return hundreds[firstdigit] + " and " + Tens (seconddigit);
+      if (input / 1000 > 0) {
+         result += NumberToWords (input / 1000) + "Thousand ";
+         input %= 1000;
       }
-
-      string[] thousands = { "", "One Thousand", "Two Thousand", "Three Thousand", "Four Thousand", "Five Thousand", "Six Thousand", "Seven Thousand", "Eight Thousand", "Nine Thousand" };
-
-      string Thousands (int input) {
-         int firstdigit = input / 1000;
-         int seconddigit = input % 1000;
-         if (seconddigit == 0) {
-            return thousands[firstdigit];
-         } else {
-            return thousands[firstdigit] + " " + Hundreds (seconddigit);
-         }
-
-
+      if (input / 100 > 0) {
+         result += NumberToWords (input / 100) + "Hundred ";
+         input %= 100;
+         if (input > 0) result += "and ";
+         else result += "";
       }
-
-      if (input == 0) {
-         return "Zero";
-      } else if (input < 100) {
-         return Tens (input);
-      } else if (input < 1000) {
-         return Hundreds (input);
-      } else if (input < 10000) {
-         return Thousands (input);
-      } else {
-         return "Input out of range";
+      if (input >= 20) {
+         result += tens[input / 10] + " ";
+         input %= 10;
       }
-
+      if (input < 20 && input > 0) result += ones[input] + " ";
+      return result;
    }
 
-
-
-
-
-
-   public static string Romanletterconverter (int input) {
-
-      string[] firstten = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" };
-      string result1 = "";
-
-
-      string numberupto39 (int input) {
-         if (input < 11) {
-            return firstten[input];
-         } else if (input > 10 && input < 40) {
-            int firstdigit = input / 10;
-            int seconddigit = input % 10;
-
-            for (int i = 1; i <= firstdigit; i++) {
-               result1 += firstten[10];
-            }
-            return result1 + firstten[seconddigit];
-         } else return "";
-      }
-
-      string numberupto90 (int input) {
-         if (input > 39 && input < 50) {
-
-            int seconddigit = input % 10;
-            return "XL" + numberupto39 (seconddigit);
-         } else if (input > 49 && input < 90) {
-
-            int seconddigit = input % 50;
-            return "L" + numberupto39 (seconddigit);
-         } else if (input > 89 && input < 100) {
-
-            int seconddigit = input % 10;
-            return "XC" + numberupto39 (seconddigit);
-         } else {
-            return numberupto39 (input);
-         }
-      }
-      string result2 = "";
-
-      string numberupto400 (int input) {
-         if (input > 99 && input < 400) {
-
-            int firstdigit = input / 100;
-            int seconddigit = input % 100;
-
-            for (int i = 1; i <= firstdigit; i++) {
-
-               result2 += "C";
-
-            }
-            return result2 + numberupto90 (seconddigit);
-
-         } else return numberupto90 (input);
-      }
-
-      string upto1000 (int input) {
-         if (input > 399 && input < 500) {
-            int seconddigit = input % 100;
-            return "CD" + numberupto90 (seconddigit);
-         } else if (input > 499 && input < 900) {
-            int seconddigit = input % 500;
-            return "D" + numberupto90 (seconddigit);
-         } else if (input > 899 && input < 1000) {
-            int seconddigit = input % 100;
-            return "CM" + numberupto90 (seconddigit);
-         } else {
-            return numberupto400 (input);
-         }
-      }
-      string upto4000 (int input) {
-         if (input > 999 && input < 4000) {
-            int firstdigit = input / 1000;
-            int seconddigit = input % 1000;
-            string result3 = "";
-            for (int i = 1; i <= firstdigit; i++) {
-               result3 += "M";
-            }
-            return result3 + upto1000 (seconddigit);
-         } else {
-            return upto1000 (input);
-         }
-      }
-
-
-      if (input < 40) return numberupto39 (input);
-      else if (input > 39 && input < 100) return numberupto90 (input);
-      else if (input > 99 && input < 400) return numberupto400 (input);
-      else if (input > 399 && input < 1000) return upto1000 (input);
-      else if (input > 999 && input < 4000) return upto4000 (input);
-      else
-         return "Invalid one";
-
-   }
-
-
-
-   static void Main (string[] args) {
-
-      int n;
-      do {
-         Console.WriteLine ("What do you want to convert?\n1.Number to Words\n2.Number to Roman Letters");
-         n = int.Parse (Console.ReadLine ());
-      } while (n != 1 && n != 2);
-
-
+   /// <summary> convert number to its equivalent roman letters upto 3999</summary>
+   /// <param name="input">Takes Natural numbers as input from the user</param>
+   /// <returns>Roman letters equivalent to the decimal</returns>
+   static string RomanLetterConverter (int input) {
+      string[] firstTen = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" };
       string result = "";
-      do {
-         if (n == 1) {
-            Console.WriteLine ("Enter a number between 0 and 9999:");
-            int input = int.Parse (Console.ReadLine ());
-            result = numbertowords (input);
-            Console.WriteLine ($"The Word format for the given input {input} is {result} ");
-
-         }
+      if (input >= 1000) {
+         result += new string ('M', input / 1000);
+         input %= 1000;
       }
-      while (result == "Invalid one");
-
-
-      do {
-
-         if (n == 2) {
-            Console.WriteLine ("Enter a number between 1 to 3999:");
-            int input = int.Parse (Console.ReadLine ());
-            result = Romanletterconverter (input);
-            Console.WriteLine ($"The roman letter for the given input {input} is {result} ");
-
-
-         }
-
+      if (input >= 900) {
+         result += "CM";
+         input -= 900;
       }
-      while (result == "Input out of range" || result == "Invalid one");
+      if (input >= 500) {
+         result += "D";
+         input -= 500;
+      }
+      if (input >= 400) {
+         result += "CD";
+         input -= 400;
+      }
+      if ((input>=100)) {
+         result += new string ('C', input / 100);
+         input %= 100;
+      }
+      if (input >= 90) {
+         result += "XC";
+         input -= 90;
+      }
+      if (input >= 50) {
+         result += "L";
+         input -= 50;
+      }
+      if (input >= 40) {
+         result += "XL";
+         input -= 40;
+      }
+      if ((input >= 10)) {
+         result += new string ('X', input / 10);
+         input %= 10;
+      }
+      if (input <= 9) result += firstTen[input];
+      return result;
+   }
 
-
-
-
-
-
+   static void Main () {
+      int inputChoice, input;
+      string result;
+      bool isValid;
+      do WriteLine ("What do you want to convert?\n1.Number to Words\n2.Number to Roman Letters");
+      while (!int.TryParse (ReadLine (), out inputChoice) || (inputChoice != 1 && inputChoice != 2));
+      if (inputChoice == 1) {
+         do {
+            WriteLine ("Enter a number less than 1 crore :");
+            isValid = (int.TryParse (ReadLine (), out input) && (input >= 0 && input <= 9999999));
+            if (isValid) {
+               result = NumberToWords (input);
+               Write ($"The word representation for the given input {input} is ");
+               ForegroundColor = ConsoleColor.Green;
+               Write (result);
+               ResetColor ();
+            } else WriteLine ("Invalid input, please enter a valid number.");
+         } while (!isValid);
+      }
+      if (inputChoice == 2) {
+         do {
+            WriteLine ("Enter a number between 1 to 3999:");
+            isValid = (int.TryParse (ReadLine (), out input) && (input > 0 && input < 4000));
+            if (isValid) {
+               result = RomanLetterConverter (input);
+               Write ($"The roman letter for the given input {input} is ");
+               ForegroundColor = ConsoleColor.Green;
+               Write (result);
+               ResetColor ();
+            } else WriteLine ("Invalid input, please enter a valid number.");
+         } while (!isValid);
+      }
    }
 }
-
-
-
