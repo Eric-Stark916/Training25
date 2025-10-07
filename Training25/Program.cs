c23b46982d608409d3eaf7722d82adbd219d945a
Eric <Eric.Sureshkumar@trumpf.com>
2025-10-07 11:13:39 +0530

    R03

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -5,15 +5,17 @@
 // Program.cs
 // Program on T01 branch."This code is used to convert decimal number, to binarynumber and hexadecimal".
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
 namespace Training25;
 
 internal class Program {
-   /// <summary>It is used to convert a normal decimal number to Binary number</summary>
-   /// <param name="input">Input number is taken from the user,takes only natural numbers</param>
+   /// <summary>Converts decimal to binary number</summary>
+   /// <param name="input">Input number is taken from the user,takes only Whole numbers</param>
    /// <returns>Binary number for the given decimal number</returns>
    static string BinaryConverter (int input) {
       string result = "";
-      while (input != 0) {
+      if (input == 0) return "0";
+      while (input > 0) {
          int remainder = input % 2;
          input /= 2;
          result = remainder + result;
@@ -21,26 +23,27 @@ internal class Program {
       return result;
    }
 
-   /// <summary>It is used to convert a normal decimal number to Binary number</summary>
-   /// <param name="input">Input number is taken from the user,takes only natural numbers</param>
-   /// <returns>Hexadecimal number for the given decimal number</returns>
+   /// <summary>Converts decimal to Hexadecimal</summary>
+   /// <param name="input">Input number is taken from the user,takes only Whole numbers</param>
+   /// <returns>Hexadecimal for the given decimal number</returns>
    static string HexConverter (int input) {
-      string hexChar = "0123456789ABCDEF";
+      string hexChars = "0123456789ABCDEF";
       string result = "";
-      while (input != 0) {
+      if (input == 0) return "0";
+      while (input > 0) {
          int remainder = input % 16;
          input /= 16;
-         result = hexChar[remainder] + result;
+         result = hexChars[remainder] + result;
       }
       return result;
    }
 
-   static void Main (string[] args) {
-      Console.WriteLine ("Enter a number");
-      string input = Console.ReadLine ();
-      if (int.TryParse (input, out int result) && result != 0) {
-         Console.WriteLine ($"Binary number:{BinaryConverter (result)}");
-         Console.WriteLine ($"Hex number:{HexConverter (result)}");
-      } else Console.WriteLine ("Invalid input");
+   static void Main () {
+      WriteLine ("Enter a number");
+      string? input = ReadLine ();
+      if (int.TryParse (input, out int result)) {
+         WriteLine ($"Binary number:{BinaryConverter (result)}");
+         WriteLine ($"Hex number:{HexConverter (result)}");
+      } else WriteLine ("Invalid input");
    }
 }
