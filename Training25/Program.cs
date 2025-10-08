d6fe638dfb02a39a2cfc718c6b6b2a27b94a21f7
Eric <Eric.Sureshkumar@trumpf.com>
2025-10-08 15:20:14 +0530

    R02

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -9,36 +9,31 @@ using static System.Console;
 namespace Training25;
 
 internal class Program {
-   /// <summary>Gives the Gretest common divisor for the given two numbers</summary>
-   /// <param name="a">Takes only Natural numbers</param>
-   /// <param name="b">Takes only Natural numbers</param>
-   /// <returns>Gretest common divisor </returns>
+   //Gives the Gretest common divisor for the given two numbers.
+   //Both input a and b Takes only Natural numbers.
+   //Returns Gretest common divisor.
    static int Gcd (int a, int b) {
-      int remainder;
       while (b != 0) {
-         remainder = a % b;
-         if (remainder == 1) return 1;
+         int remainder = a % b;
          a = b;
          b = remainder;
       }
       return a;
    }
 
-   /// <summary>Gives the Lowest common divisor for the given two numbers</summary>
-   /// <param name="a">Takes only Natural numbers</param>
-   /// <param name="b">Takes only Natural numbers</param>
-   /// <returns>Lowest common divisor</returns>
-   static int Lcm(int a, int b) {
-      return ((a * b) / Gcd (a, b));
-   }
+   //Gives the Lowest common divisor for the given two numbers.
+   //Both input a and b Takes only Natural numbers.
+   //Returns Lowest common divisor.
+   static int Lcm (int a, int b) => (a * b) / Gcd (a, b);
 
    static void Main () {
-      WriteLine (" What two number you need to find GCD & LCM eg:10,5: ");
-      string[] num = ReadLine()?.Split (',') ?? Array.Empty<string> ();
-      if (int.TryParse ((num[0]), out int value1) && int.TryParse ((num[1]),
-         out int value2) && num.Length == 2 && (value1 > 0 && value2 > 0)) {
+      WriteLine ("Enter two positive integers to find their GCD and LCM.");
+      string? num1 = ReadLine ();
+      string? num2 = ReadLine ();
+      if (int.TryParse (num1, out int value1) && int.TryParse (num2, out int value2)
+         && (value1 > 0 && value2 > 0)) {
          WriteLine ($"The GCD of the given input {value1},{value2} is:" + Gcd (value1, value2));
          WriteLine ($"The LCM of the given input {value1},{value2} is:" + Lcm (value1, value2));
-      } else WriteLine ("Invalid input, please enter two positive integers separated by a comma.");
+      } else WriteLine ("Invalid input, please enter two positive integers ");
    }
 }
