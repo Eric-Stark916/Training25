df49fe07917661345b9cd254abbfd4880a18bcca
Eric <Eric.Sureshkumar@trumpf.com>
2025-10-09 15:39:36 +0530

    R02

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -5,42 +5,34 @@
 // Program.cs
 // Program on branch T07.//This code creates a Pascal's Triangle up to a specified number of rows.
 // ------------------------------------------------------------------------------------------------
-using System.Numerics;
+using static System.Console;
 namespace Training25;
 
 internal class Program {
-   /// <summary>This method is used to find out the factorial of a number</summary>
-   static BigInteger Factorial (BigInteger number) {
-      BigInteger count = 1; BigInteger result = number;
-      if (number == 0 || (number == 1)) return 1;
-      else while (count < number) result *= (number - count++);
-      return result;
-   }
-
-   /// <summary>This method is used to find out the numbers in different position example(2,0),(2,1)</summary>
-   /// <param name="number">It is the number of rows in a Pascal's Triangle</param>
-   /// <param name="position">It is the coloumn for the Pascal's Triangle</param>
-   static BigInteger PascalStructure (BigInteger number, BigInteger position) {
-      return Factorial (number) / (Factorial (position) * Factorial (number - position));
-   }
-
-   static void Main (string[] args) {
-      Console.WriteLine ("Enter number of rows required in Pascal's Triangle:");
-      if (!int.TryParse (Console.ReadLine (), out int num) || num < 0 || num > 20) {
-         Console.WriteLine ("Invalid Input");
-         return;
+   // Prints Pascal's triangle
+   static int PascalTriangle (int rowLimit) {
+      int value = 0;
+      for (int row = 0; row < rowLimit; row++) {                       // for Row traverse
+         value = 1;
+         for (int gap = 0; gap < rowLimit - row; gap++) Write (" ");  // for spaces in decending order to form the triangle shape
+         for (int col = 0; col <= row; col++) {                      // for coloumn traverse
+            Write ($"{value} ");
+            value = value * (row - col) / (col + 1);
+         }
+         WriteLine ();                                             // New line after each row
       }
-      for (int i = 0; i < num; i++) {
-         for (int j = 0; j < num - i; j++) Console.Write (" ");//for spaces to form the triangle shape
-         for (int k = 0; k <= i; k++) Console.Write (PascalStructure (i, k) + "  ");//to print the numbers in the triangle
-         Console.WriteLine ();
+      return value;
+   }
+
+   static void Main () {
+      bool isValid = false;
+      while (!isValid) {
+         WriteLine ("Enter number of rows required in Pascal's Triangle:");
+         if (int.TryParse (ReadLine (), out int output) && output > 0 &&
+            output < 20) {
+            isValid = true;
+            PascalTriangle (output);
+         } else WriteLine ("Invalid Input,Enter a valid one");
       }
    }
 }
-
-
-
-
-
-
-
