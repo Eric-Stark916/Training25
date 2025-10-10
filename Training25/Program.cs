e812c5c45d77a09a0619ef3e435ca4728eeb4e9a
Eric <Eric.Sureshkumar@trumpf.com>
2025-10-10 12:59:45 +0530

    R03

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,122 +3,95 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on branch T02 //This code is used to convert numbers to its equivalent words and Romanletters.
+// Program on branch T02: Number to words and Roman numeral converter.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 namespace Training25;
 
 internal class Program {
-   /// <summary>convert nuber to words upto 9999999</summary>
-   /// <param name="input">Takes whole number as input </param>
-   /// <returns>words equivalent to the number entered</returns>
-   static string NumberToWords (int input) {
-      string[] ones = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+   // Converts a number to words,upto number 9999999.
+   static string NumToWords (int inp) {
+      if (inp == 0) return "Zero";
+      string[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
                              "Nine","Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
-                             "Sixteen", "Seventeen", "Eighteen", "Nineteen", };
-      string[] tens = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy",
-                                 "Eighty", "Ninety" };
+                             "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+      string[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy",
+                                 "Eighty", "Ninety"};
       String result = "";
-      if (input == 0) return "Zero";
-      if (input / 100000 > 0) {
-         result += NumberToWords (input / 100000) + "Lakh ";
-         input %= 100000;
+      int lakhs = inp / 100000;
+      if (lakhs > 0) {
+         result += NumToWords (lakhs) + "Lakh ";
+         inp %= 100000;
       }
-      if (input / 1000 > 0) {
-         result += NumberToWords (input / 1000) + "Thousand ";
-         input %= 1000;
+      int thousands = inp / 1000;
+      if (thousands > 0) {
+         result += NumToWords (thousands) + "Thousand ";
+         inp %= 1000;
       }
-      if (input / 100 > 0) {
-         result += NumberToWords (input / 100) + "Hundred ";
-         input %= 100;
-         if (input > 0) result += "and ";
-         else result += "";
+      int hundreds = inp / 100;
+      if (hundreds > 0) {
+         result += NumToWords (hundreds) + "Hundred ";
+         inp %= 100;
+         if (inp > 0) result += "and ";
       }
-      if (input >= 20) {
-         result += tens[input / 10] + " ";
-         input %= 10;
+      if (inp >= 20) {
+         result += tens[inp / 10] + " ";
+         inp %= 10;
       }
-      if (input < 20 && input > 0) result += ones[input] + " ";
+      if (inp < 20) result += ones[inp] + " ";
       return result;
    }
 
-   /// <summary> convert number to its equivalent roman letters upto 3999</summary>
-   /// <param name="input">Takes Natural numbers as input from the user</param>
-   /// <returns>Roman letters equivalent to the decimal</returns>
-   static string RomanLetterConverter (int input) {
-      string[] firstTen = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" };
+   // Converts a number to its equivalent Roman numeral,upto number 3999.
+   static string RomNumConv (int inp) {
       string result = "";
-      if (input >= 1000) {
-         result += new string ('M', input / 1000);
-         input %= 1000;
+      Dictionary<int, string> romNum = new () {
+         {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+         {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+         {10, "X"}, {9, "IX"}, {8, "VIII"}, {7, "VII"}, {6, "VI"}, {5, "V"}, {4, "IV"},
+         {3, "III"}, {2, "II"}, {1, "I"},
+       };
+      foreach (var item in romNum) {
+         while (inp >= item.Key) {
+            result += item.Value;
+            inp -= item.Key;
+         }
       }
-      if (input >= 900) {
-         result += "CM";
-         input -= 900;
-      }
-      if (input >= 500) {
-         result += "D";
-         input -= 500;
-      }
-      if (input >= 400) {
-         result += "CD";
-         input -= 400;
-      }
-      if ((input>=100)) {
-         result += new string ('C', input / 100);
-         input %= 100;
-      }
-      if (input >= 90) {
-         result += "XC";
-         input -= 90;
-      }
-      if (input >= 50) {
-         result += "L";
-         input -= 50;
-      }
-      if (input >= 40) {
-         result += "XL";
-         input -= 40;
-      }
-      if ((input >= 10)) {
-         result += new string ('X', input / 10);
-         input %= 10;
-      }
-      if (input <= 9) result += firstTen[input];
       return result;
    }
 
    static void Main () {
-      int inputChoice, input;
-      string result;
-      bool isValid;
-      do WriteLine ("What do you want to convert?\n1.Number to Words\n2.Number to Roman Letters");
-      while (!int.TryParse (ReadLine (), out inputChoice) || (inputChoice != 1 && inputChoice != 2));
-      if (inputChoice == 1) {
-         do {
-            WriteLine ("Enter a number less than 1 crore :");
-            isValid = (int.TryParse (ReadLine (), out input) && (input >= 0 && input <= 9999999));
-            if (isValid) {
-               result = NumberToWords (input);
-               Write ($"The word representation for the given input {input} is ");
-               ForegroundColor = ConsoleColor.Green;
-               Write (result);
-               ResetColor ();
-            } else WriteLine ("Invalid input, please enter a valid number.");
-         } while (!isValid);
+      int inpChoice, inp;
+      string result = "";
+      bool isVal = false;
+      do WriteLine ("What do you want to convert?\n1.Number to words\n2.Number to Roman numeral");
+      while (!int.TryParse (ReadLine (), out inpChoice) || (inpChoice != 1 && inpChoice != 2));      // Validating inp choice common for both conversions.
+      if (inpChoice == 1) {                                                                         // For number to words
+         while (!isVal) {
+            Write ("Enter a number less than 1 crore: ");
+            isVal = int.TryParse (ReadLine (), out inp) && inp >= 0 && inp <= 9999999;
+            if (isVal) {
+               result = NumToWords (inp);
+               Write ($"The word representation for the given input {inp} is: ");
+            } else if (inp < 0 || inp > 9999999) isVal = false;
+            else Write ("Invalid input.");
+         }
       }
-      if (inputChoice == 2) {
-         do {
-            WriteLine ("Enter a number between 1 to 3999:");
-            isValid = (int.TryParse (ReadLine (), out input) && (input > 0 && input < 4000));
-            if (isValid) {
-               result = RomanLetterConverter (input);
-               Write ($"The roman letter for the given input {input} is ");
-               ForegroundColor = ConsoleColor.Green;
-               Write (result);
-               ResetColor ();
-            } else WriteLine ("Invalid input, please enter a valid number.");
-         } while (!isValid);
+      if (inpChoice == 2) {                                                                       // For Roman numerals
+         while (!isVal) {
+            Write ("Enter a number between 1 to 3999: ");
+            isVal = (int.TryParse (ReadLine (), out inp)) && (inp > 0 && inp < 4000);
+            if (isVal) {
+               result = RomNumConv (inp);
+               Write ($"The Roman numeral for the given input {inp} is: ");
+            } else if ((inp < 1 || inp > 3999)) isVal = false;
+            else Write ("Invalid input.");
+         }
+      }
+      if (isVal) {                                                                            // To display the result in green color common for both conversions.
+         ForegroundColor = ConsoleColor.Green;
+         Write (result);
+         ResetColor ();
       }
    }
 }
