6e7d27fe3fbbf9078c18254857f80eaaabcc1283
Eric <Eric.Sureshkumar@trumpf.com>
2025-10-13 11:34:45 +0530

    R04

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,33 +3,32 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on branch T03 //This code is used to find GCD and LCM of two numbers.
+// Program on branch T03: LCM & GCD Generator
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 namespace Training25;
 
 internal class Program {
-   // Gives the Gretest common divisor for the given two numbers.
-   static int Gcd (int a, int b) {
+   // Gives the greatest common divisor of a and b
+   static int GCD (int a, int b) {
       while (b != 0) {
-         int remainder = a % b;
+         int rem = a % b;
          a = b;
-         b = remainder;
+         b = rem;
       }
       return a;
    }
 
-   // Gives the Lowest common divisor for the given two numbers.
-   static int Lcm (int a, int b) => (a * b) / Gcd (a, b);
-
    static void Main () {
-      WriteLine ("Enter two positive integers to find their GCD and LCM.");
+      WriteLine ("Enter two positive integers to find their GCD and LCM: ");
       string? num1 = ReadLine ();
       string? num2 = ReadLine ();
-      if (int.TryParse (num1, out int value1) && int.TryParse (num2, out int value2)
-         && (value1 > 0 && value2 > 0)) {
-         WriteLine ($"The GCD of the given input {value1},{value2} is:" + Gcd (value1, value2));
-         WriteLine ($"The LCM of the given input {value1},{value2} is:" + Lcm (value1, value2));
-      } else WriteLine ("Invalid input, please enter two positive integers ");
+      if (int.TryParse (num1, out int val1) && int.TryParse (num2, out int val2)
+         && val1 > 0 && val2 > 0) {
+         int gcd = GCD (val1, val2);
+         int lcm = (val1 * val2) / gcd;
+         WriteLine ($"The GCD of the given input {val1}, {val2} is: " + gcd);
+         WriteLine ($"The LCM of the given input {val1}, {val2} is: " + lcm);
+      } else WriteLine ("Invalid input, please enter two positive integers");
    }
 }
