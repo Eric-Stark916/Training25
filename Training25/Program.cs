8b2650a06c1a3385b5f497dc570c5f7c9c8bdd54
Eric <Eric.Sureshkumar@trumpf.com>
2025-10-16 12:23:35 +0530

    R05

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,97 +3,82 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on branch T02: Number to words and Roman numeral converter.
+// Program on branch T02: Number to Words and Roman Numeral Converter.
 // ------------------------------------------------------------------------------------------------
+using System.Text;
 using static System.Console;
 namespace Training25;
 
 internal class Program {
-   // Converts a number to words,upto number 9999999.
+   static void Main () {
+      while (true) {
+         WriteLine ("Welcome to number conversion game. Choose mode: \n1. Words \n2. Roman Numerals");
+         Write ("Enter your choice (1 or 2) or 'X' to close: ");
+         string? input = ReadLine ();
+         if (input == "x") break;
+         if (!int.TryParse (input, out int choice) || choice != 1 && choice != 2) {
+            WriteLine ("Invalid choice.");
+            continue;
+         }
+         (int min, int max) = choice == 1 ? (0, 9999999) : (1, 3999);
+         Write ($"Enter a number between {min} and {max} to convert: ");
+         bool inValid = !int.TryParse (ReadLine (), out int num) || num < min || num > max;
+         WriteLine (inValid ? "Invalid input. Number must be with in the specified range." :
+            choice == 1 ? $"The word representation for the given input {num} is: {NumToWords (num)}" :
+            $"The Roman numeral for the given input {num} is: {RomNumConv (num)}");
+         WriteLine ("---------------");
+      }
+   }
+
+   // Converts a number (from 1 to 9999999) into its word representation.
    static string NumToWords (int inp) {
       if (inp == 0) return "Zero";
-      string[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
-                             "Nine","Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
-                             "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-      string[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy",
-                                 "Eighty", "Ninety"};
-      String result = "";
-      int lakhs = inp / 100000;
-      if (lakhs > 0) {
-         result += NumToWords (lakhs) + "Lakh ";
-         inp %= 100000;
+      List<string> words = [ "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+                             "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
+                             "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty", "Thirty", 
+                             "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" ];
+      StringBuilder result = new ();
+      int lakh = 100000, thousand = 1000, hundred = 100, ten = 10, zero = 0;
+      int hundreds = 0, lakhs = 0, thousands = 0;
+      AddWord (lakhs, "Lakhs", lakh);
+      AddWord (thousands, "Thousand", thousand);
+      AddWord (hundreds, "Hundred", hundred);
+      if (inp > 20) {
+         result.Append (words[18 + (inp / ten)]);
+         inp %= ten;
+         // For numbers like 21, 32, 43 etc.
+         if (inp > zero) result.Append (" " + words[inp]);
       }
-      int thousands = inp / 1000;
-      if (thousands > 0) {
-         result += NumToWords (thousands) + "Thousand ";
-         inp %= 1000;
-      }
-      int hundreds = inp / 100;
-      if (hundreds > 0) {
-         result += NumToWords (hundreds) + "Hundred ";
-         inp %= 100;
-         if (inp > 0) result += "and ";
-      }
-      if (inp >= 20) {
-         result += tens[inp / 10] + " ";
-         inp %= 10;
-      }
-      if (inp < 20) result += ones[inp] + " ";
-      return result;
-   }
+      // For numbers from 1 to 20
+      else if (inp > zero) result.Append (words[inp]);
+      return result.ToString ();
 
-   // Converts a number to its equivalent Roman numeral,upto number 3999.
-   static string RomNumConv (int inp) {
-      string result = "";
-      Dictionary<int, string> romNum = new () {
-         {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-         {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-         {10, "X"}, {9, "IX"}, {8, "VIII"}, {7, "VII"}, {6, "VI"}, {5, "V"}, {4, "IV"},
-         {3, "III"}, {2, "II"}, {1, "I"},
-       };
-      foreach (var item in romNum) {
-         while (inp >= item.Key) {
-            result += item.Value;
-            inp -= item.Key;
+      // Helper-----------------------------------------
+      void AddWord (int num, string word, int div) {
+         num = inp / div;
+         if (num > zero) {
+            result.Append ($"{NumToWords (num)} {word}");
+            inp %= div;
+            if (inp > zero && div == hundred) result.Append (" and ");
+            else if (inp > zero) result.Append (' ');
          }
       }
-      return result;
    }
 
-   static void Main () {
-      int inpChoice, inp;
-      string result = "";
-      bool isVal = false;
-      do WriteLine ("What do you want to convert?\n1.Number to words\n2.Number to Roman numeral");
-      while (!int.TryParse (ReadLine (), out inpChoice) || (inpChoice != 1 && inpChoice != 2));
-      switch (inpChoice) {
-         case 1:
-            while (!isVal) {
-               Write ("Enter a number less than 1 crore: ");
-               isVal = int.TryParse (ReadLine (), out inp) && inp >= 0 && inp <= 9999999;
-               if (isVal) {
-                  result = NumToWords (inp);
-                  Write ($"The word representation for the given input {inp} is: ");
-               } // After 1 crore the wordings are not correct
-               else if (inp < 0 || inp > 9999999) isVal = false;
-            }
-            break;
-         case 2:
-            while (!isVal) {
-               Write ("Enter a number between 1 to 3999: ");
-               isVal = (int.TryParse (ReadLine (), out inp)) && (inp > 0 && inp < 4000);
-               if (isVal) {
-                  result = RomNumConv (inp);
-                  Write ($"The Roman numeral for the given input {inp} is: ");
-               }// There are no Roman numerals for 0 and negative numbers and >3999
-               else if ((inp < 1 || inp > 3999)) isVal = false;
-            }
-            break;
+   // Converts an number (from 1 to 3999) into its Roman numeral representation.
+   static string RomNumConv (int inp) {
+      StringBuilder result = new ();
+      var romNum = new List<(int, string)> {
+                 (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+                  (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+                   (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+      };
+      foreach (var (num, rom) in romNum) {
+         while (inp >= num) {
+            result.Append (rom);
+            inp -= num;
+         }
       }
-      if (isVal) { // To display the result in green color common for both conversions.                                                                    
-         ForegroundColor = ConsoleColor.Green;
-         Write (result);
-         ResetColor ();
-      }
+      return result.ToString ();
    }
 }
