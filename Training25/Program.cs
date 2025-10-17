f0d4815cc8d775036ef62664d81fa084dac52ba5
Eric <Eric.Sureshkumar@trumpf.com>
2025-10-17 10:29:55 +0530

    R04

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -10,14 +10,11 @@ namespace Training25;
 
 internal class Program {
    static void Main () {
-      bool isValid = false;
-      while (!isValid) {
+      while (true) {
          WriteLine ("Enter the password: ");
-         string password = ReadLine () ?? "";
-         if (!string.IsNullOrEmpty (password)) {
-            isValid = true;
-            if (!Password.IsPasswordValid (Password.ConditionsSatisfied (password))) isValid = false;
-         } else WriteLine ("Invalid input");
+         string pass = ReadLine () ?? "";
+         Write (Password.PasswordValidator (pass));
+         if (Password.PasswordValidator (pass) == "Strong Password.") break;
       }
    }
 }
@@ -25,41 +22,23 @@ internal class Program {
 static class Password {
    /// <summary>Validates the password against multiple conditions</summary>
    /// <param name="input">The password to validate</param>
-   /// <returns>The number of conditions satisfied by the password</returns>
-   public static int ConditionsSatisfied (string input) {
+   /// <returns>Strong or weak password and also the conditions to be addressed</returns>
+   public static string PasswordValidator (string inp) {
+      List<string> condi = [];
       char[] chars = { '!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '+', '^' };
-      int count = 0;
-      WriteLine ("...............");
-      if (input.Length < 6) WriteLine ("*Password length should be at least 6 characters");
-      else count++;
-      if (!input.Any (char.IsDigit)) WriteLine ("*Password should contain at least one digit");
-      else count++;
-      if (!input.Any (char.IsUpper)) WriteLine ("*Password should contain at least one " +
-                                                                          "uppercase letter");
-      else count++;
-      if (!input.Any (char.IsLower)) WriteLine ("*Password should contain at least one " +
-                                                                          "lowercase letter");
-      else count++;
-      if (input.IndexOfAny (chars) == -1) WriteLine ("*Password should contain at least one " +
-                                                                         "special character");
-      else count++;
-      return count;
-   }
-
-   /// <summary>Evaluates the password strength based on the number of conditions satisfied</summary>
-   /// <param name="count">The number of conditions satisfied by the password</param>
-   /// <returns>True if password is valid</returns>
-   public static bool IsPasswordValid (int count) {
-      bool result = false;
-      if (count <= 4) {
-         ForegroundColor = ConsoleColor.Red;
-         WriteLine ("Weak password\nTry again");
-      } else {
-         ForegroundColor = ConsoleColor.Green;
-         WriteLine ("Strong password\nYour password is valid");
-         result = true;
+      if (string.IsNullOrWhiteSpace (inp)) condi.Add ("*Password should not be empty.");
+      else {
+         if (inp.Any (char.IsWhiteSpace)) condi.Add ("*There should be no space in the whole password.");
+         if (inp.Length < 6) condi.Add ("*Password length should be at least 6 characters.");
+         if (!inp.Any (char.IsDigit)) condi.Add ("*Password should contain at least one digit.");
+         if (!inp.Any (char.IsUpper)) condi.Add ("*Password should contain at least one " +
+                                                                             "uppercase letter.");
+         if (!inp.Any (char.IsLower)) condi.Add ("*Password should contain at least one " +
+                                                                             "lowercase letter.");
+         if (inp.IndexOfAny (chars) == -1) condi.Add ("*Password should contain at least one " +
+                                                                            "special character.");
       }
-      ResetColor ();
-      return result;
+      return condi.Count == 0 ? "Strong Password." : "Weak Password.Try again.\nHints:\n" +
+                                                     string.Join ("\n", condi) + "\n\n";
    }
 }
