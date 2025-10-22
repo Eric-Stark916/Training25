7c12acb0e9e941c49fb478d4123efff021669221
Eric <Eric.Sureshkumar@trumpf.com>
2025-10-22 15:50:19 +0530

    Initial Commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,32 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on branch T06:Digital Root of a Number.
 // ------------------------------------------------------------------------------------------------
-namespace Training25;
-internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+using static System.Console;
+namespace Training25 {
+
+   internal class Program {
+      static void Main () {
+         while (true) {
+            Write ("Enter a number to find its digital root or '-1' to exit: ");
+            bool isValid = int.TryParse (ReadLine (), out int num) && num >= 0;
+            if(num== -1) break;
+            WriteLine (isValid ? $"=>The digital root of {num} is {DigitalRoot (num)}\n" : "Invalid input.Try again\n");
+         }
+      }
+
+      // Function to calculate Digital Root of a number
+      static int DigitalRoot (int inp) {
+         while (true) {
+            // Separates the digits except the last digit.
+            int div = inp / 10;
+            // Separates the last digit.
+            int rem = inp % 10;
+            inp = div + rem;
+            if (inp < 10) break;
+         }
+         return inp;
+      }
    }
 }
