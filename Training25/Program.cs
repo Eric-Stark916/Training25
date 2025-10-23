69985cce758fcf9e66ce29716c73f68e9cc5ecb6
Eric <Eric.Sureshkumar@trumpf.com>
2025-10-23 09:13:01 +0530

    Initialcommit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,32 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on branch T09: Reduced String.
 // ------------------------------------------------------------------------------------------------
+using System.Text;
+using static System.Console;
 namespace Training25;
+
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+
+   static void Main () {
+      while (true) {
+         Write ("Enter characters to delete the matching adjacent character: ");
+         string? inp = ReadLine ();
+         if (string.IsNullOrEmpty (inp)) continue;
+         WriteLine ($"The reduced string for the given input {inp} is {ReducedString (inp)}\n");
+      }
+
+      // Checks and removes the adjacent matching characters from the string.
+      static string ReducedString (string inp) {
+         StringBuilder chars = new (inp);
+         for (int i = 0; i < chars.Length; i++) {
+            if (i > 0 && chars[i] == chars[i - 1]) {
+               chars.Remove (i, 1);
+               i = 0;
+            }
+         }
+         return chars.ToString ();
+      }
    }
 }
