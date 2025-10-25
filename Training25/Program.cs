938af4e8f9aaa42c5c05c2cf68c2725fc770f6d6
Eric <Eric.Sureshkumar@trumpf.com>
2025-10-25 12:16:42 +0530

    R01

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,46 +3,36 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on branch T05: Multiplication Tables 
+// Program on branch T05: Multiplication Tables.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 namespace Training25;
 
+#region class Program -----------------------------------------------------------------------------
 internal class Program {
+   #region Implementation -------------------------------------------
    static void Main () {
       while (true) {
-         WriteLine ("Enter the number of tables you want to print (max 100): ");
-         bool isvalid = int.TryParse (ReadLine (), out int inp);
-         if (isvalid && inp < 101) {// After 100, output is not in proper alignment
-            Tables (inp);
+         Write ("Enter the number of tables you want to print: ");
+         bool isValid = int.TryParse (ReadLine (), out int inp);
+         if (isValid) {
+            MultiplicationTables (inp);
             break;
-         } else { WriteLine ("Invalid input"); }
+         }
+         WriteLine ("Invalid input. Try again.");
       }
    }
 
-   // Function to print multiplication tables with header
-   static void Tables (int inpCol) {
-      int row = 5;// Row limit
-      // For starting new row of tables after row limit
-      for (int col = 1; col <= inpCol; col += row) {
-         WriteLine ();
-         // For printing each table header
-         for (int hed = col; hed < col + row && hed <= inpCol; hed++) {
-            ForegroundColor = ConsoleColor.Cyan;
-            Write ($"  [Tables {hed}]   ");// Spaces for alignment
-            ResetColor ();
-         }
-         WriteLine ();
-         WriteLine ();
-         // For printing multiplication number
-         for (int mulNo = 1; mulNo <= 10; mulNo++) {
-            // For printing table number
-            for (int tabNo = col; tabNo < col + row && tabNo <= inpCol; tabNo++)
-               // Space at the starting is for alignment
-               Write ($"  {mulNo,2} X {tabNo,-2} = {tabNo * mulNo,-3}");
-            WriteLine ();
-         }
-         WriteLine ();
+   // Function to print multiplication tables with header.
+   static void MultiplicationTables (int tableNo) {
+      // For printing multiplication number.
+      for (int tabNo = 1; tabNo <= tableNo; tabNo++) {
+         WriteLine ($"\n  Table {tabNo}\n");
+         // For printing table number.
+         for (int mulNo = 1; mulNo <= 10; mulNo++)
+            WriteLine ($"{tabNo,3} * {mulNo,2} = {tabNo * mulNo}");
       }
    }
+   #endregion
 }
+#endregion
