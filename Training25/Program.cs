a36e50cdd286777926aa1bc872eb31cbb023f09e
Eric <Eric.Sureshkumar@trumpf.com>
2025-10-27 15:06:24 +0530

    R02

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -6,32 +6,31 @@
 // Program on branch T06: Digital Root of a Number.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
-namespace Training25 {
+namespace Training25;
 
-   #region class Program --------------------------------------------------------------------------
-   internal class Program {
-      #region Implementation ----------------------------------------
-      static void Main () {
-         while (true) {
-            Write ("Enter a number to find its digital root or 'X' to exit: ");
-            var input = ReadLine ()?.Trim ().ToLower ();
-            if (input == "x") break;
-            bool isValid = int.TryParse (input, out int num) && num >= 0;
-            WriteLine (isValid ? $"The digital root of {num} is {DigitalRoot (num)}\n" : "Invalid input. Try again.\n");
-         }
+#region class Program --------------------------------------------------------------------------
+internal class Program {
+   #region Implementation ----------------------------------------
+   static void Main () {
+      while (true) {
+         Write ("Enter a number to find its digital root or 'X' to exit: ");
+         var input = ReadLine ()?.Trim ().ToLower ();
+         if (input == "x") break;
+         bool isValid = int.TryParse (input, out int num) && num >= 0;
+         WriteLine (isValid ? $"The digital root of {num} is {DigitalRoot (num)}\n" : "Invalid input. Try again.\n");
       }
+   }
 
-      // Function to calculate Digital Root of a number.
-      static int DigitalRoot (int inp) {
-         while (inp >= 10) {
-            // Separates the digits except the last digit.
-            int div = inp / 10;
-            // Separates the last digit.
-            int rem = inp % 10;
-            inp = div + rem;
-         }
-         return inp;
+   // Gets the digital root of a number.
+   static int DigitalRoot (int inp) {
+      while (inp >= 10) {
+         // Separates the digits except the last digit.
+         int div = inp / 10;
+         // Separates the last digit.
+         int rem = inp % 10;
+         inp = div + rem;
       }
+      return inp;
    }
    #endregion
 }
