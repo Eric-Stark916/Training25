23229ccb4471db456a8f07b04292acff92c6a5dd
Eric <Eric.Sureshkumar@trumpf.com>
2025-10-29 16:38:11 +0530

    R01

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -15,39 +15,37 @@ internal class Program {
       while (true) {
          Write ("Enter a number to see transformation steps, or 'X' to exit or 'I' for information: ");
          var input = ReadLine ();
-         if (input?.ToLower ().Trim () == "i") {
+         if (input?.Trim ().ToLower () == "i") {
             WriteLine ("\n1)This program calculates the smallest number of steps required to" +
-                       "\ntransform all digits of a given number into a same digit." +
+                       "\ntransform all digits from a given number into the same digit." +
                        "\n2)The transformed digit must be with in the given number." +
                        "\n3)Each step consists of incrementing or decrementing a digit by 1." +
-                       "\nExample: 399 -> 999 -> 6 Steps.\n");
+                       "\nExample: 399 -> 999 -> 6 steps.\n");
             continue;
          }
-         if (input?.ToLower ().Trim () == "x") break;
+         if (input?.Trim ().ToLower () == "x") break;
          bool isvalid = int.TryParse (input, out var num) && num >= 0;
          WriteLine (isvalid ? $"The smallest possible steps required to change the number {input} is: " +
-                              $"{smallest (num)}\n" : "Invalid input.\n");
+                              $"{GetMinSteps (num)}\n" : "Invalid input.\n");
       }
    }
 
-   // This method finds the digit in the number that requires the least total steps to convert all other digits to it.
-   static int smallest (int inp) {
-      List<int> numList = [];
+   // finds the digit in the number that requires the least total steps to convert all other digits to it.
+   static int GetMinSteps (int inp) {
       if (inp < 10) return 0;
-      while (inp > 0) {
-         int rem = inp % 10;
-         numList.Add (rem);
-         inp = inp / 10;
-      }
       int minSteps = int.MaxValue;
-      for (int i = 0; i < numList.Count; i++) {
+      for (int i = 0; i <= 9; i++) {
          int steps = 0;
-         for (int j = 0; j < numList.Count; j++)
-            steps += Math.Abs (numList[i] - numList[j]);
-         if (steps < minSteps) minSteps = steps;
+         int tmp = inp;
+         while (tmp > 0) {
+            int digit = tmp % 10;
+            steps += Math.Abs (digit - i);
+            tmp /= 10;
+         }
+         minSteps = Math.Min (minSteps, steps);
       }
       return minSteps;
+      #endregion
    }
-   #endregion
 }
 #endregion
