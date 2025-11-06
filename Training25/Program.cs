998bd3d804c946b16f25df0235bc44e3e8c4bc33
Eric <Eric.Sureshkumar@trumpf.com>
2025-11-06 15:26:26 +0530

    Initialcommit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,42 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on branch T11.1: Nth Armstrong Number.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
 namespace Training25;
+
+#region class Program -----------------------------------------------------------------------------
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+   #region Implementation -------------------------------------------
+   static void Main () {
+      while (true) {
+         Write ("Enter a number (1–25) to find the nth Armstrong number, or 'X' to exit: ");
+         var inp = ReadLine ()?.Trim ().ToLower ();
+         if (inp == "x") break;
+         else if (int.TryParse (inp, out int nthPlace) && nthPlace > 0 && nthPlace < 26)
+            WriteLine ($"The {nthPlace}th Armstrong number is: {(nthPlace <= 10 ? nthPlace - 1 : GetArmstrongNum (nthPlace))}\n");
+      }
    }
+
+   // Returns the nth Armstrong number.
+   static string GetArmstrongNum (int nthPlace) {
+      int num = 153, placeCount = 10;
+      while (true) {
+         if (IsArmstrongNum (num)) placeCount++;
+         if (placeCount == nthPlace) break;
+         num++;
+      }
+      return num.ToString ();
+   }
+
+   // Finds whether a number is an Armstrong number or not.
+   static bool IsArmstrongNum (int inp) {
+      int len = inp.ToString ().Length, sum = 0;
+      for (int temp = inp; temp > 0; temp = temp / 10)
+         sum += (int)Math.Pow (temp % 10, len);
+      return sum == inp;
+   }
+   #endregion
 }
+#endregion
