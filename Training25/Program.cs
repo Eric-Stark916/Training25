96c199b7a297d94fae5842fb8baf1b50b1cf4515
Eric <Eric.Sureshkumar@trumpf.com>
2025-11-14 15:30:29 +0530

    R03

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -24,16 +24,17 @@ internal class Program {
 
    // Returns the winner character based on maximum votes.
    static char GetWinner (string input) {
-      int currentWinner = 0;
+      int maxVotes = 0;
       char winner = ' ';
       Dictionary<char, int> charVotes = [];
-      foreach (var chars in input) {
+      foreach (var letter in input) {
          // Stores the character with default value 1.
-         charVotes[chars] = charVotes.GetValueOrDefault (chars, 0) + 1;
+         int voteCount = charVotes.GetValueOrDefault (letter, 0) + 1;
+         charVotes[letter] = voteCount;
          // In case of tie, the first character with maximum votes is considered as winner.
-         if (charVotes.Values.Max () > currentWinner) {
-            currentWinner = charVotes.Values.Max ();
-            winner = chars;
+         if (voteCount > maxVotes) {
+            maxVotes = voteCount;
+            winner = letter;
          }
       }
       return winner;
