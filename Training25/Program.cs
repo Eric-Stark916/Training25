01fe705c48c90f907e392032526716c7389dad31
Eric <Eric.Sureshkumar@trumpf.com>
2025-11-18 11:24:17 +0530

    R01

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -12,55 +12,36 @@ namespace Training25;
 internal class Program {
    #region Implementation -------------------------------------------
    static void Main () {
+      char splLetter,orderType; string? input;
       while (true) {
-         Write ("Enter the array of letters,special letters and order A or D each should be separated by comma or enter 'I' for information: ");
-         List<string> words = [.. (ReadLine () ?? "").ToLower ().Split (',')];
-         if (words[0] == "i")
-            WriteLine ("\nThe input should be in the following order: abcd,c,d\n" +
-                "Here, 'abcd' is the letters to be sorted.\n" +
-                "'c' is the special letter that will not be included in the sorting process and will appear at the end.\n" +
-                "'d' indicates descending order sorting, entering 'a' instead will sort in ascending order.\n" +
-                "The output for the above input will look like this: \"d,b,a,c\".\n");
-         else {
-            if (words.Count != 3) {
-               WriteLine ("\nPlease enter exactly 3 values separated by commas\n");
-               continue;
-            }
-            if (words.Any (w => w.Any (c => char.IsDigit (c) || char.IsWhiteSpace (c)))) {
-               WriteLine ("\nThere should be no number and space!\n");
-               continue;
-            }
-            while (words[1].Length != 1 || !char.IsLetter (words[1][0])) {
-               WriteLine ("\nOnly one special letter should be used!\n");
-               Write ("Update the special letter: ");
-               words[1] = (ReadLine () ?? "").ToLower ();
-            }
-            while (words[2] != "d" && words[2] != "a") {
-               WriteLine ("\nThe order should either contain A or D.\n");
-               Write ("Update the order: ");
-               words[2] = (ReadLine () ?? "").ToLower ();
-            }
-            WriteLine ($"The resultant array: \"{GetSortedArray (words[0], words[1], words[2])}\"\n");
-            break;
-         }
+         Write ("Enter letters (no numbers or special characters and no spaces): ");
+         input = ReadLine ();
+         if (!string.IsNullOrEmpty (input) && input.All (ch => char.IsLetter (ch))) break;
       }
+      var letters = input.ToLower ().Split (' ').ToList ();
+      while (true) {
+         Write ("\nPress a special letter (this will be moved to the end after sorting): ");
+         splLetter = ReadKey (intercept: true).KeyChar;
+         if (char.IsLetter (splLetter)) break;
+      }
+      Write ($"{splLetter}\n");
+      while (true) {
+         Write ("\nPress sorting order (A = Ascending, D = Descending): ");
+         orderType = char.ToUpper (ReadKey (intercept: true).KeyChar);
+         if (orderType is 'A' or 'D') break;
+      }
+      WriteLine (orderType);
+      Write ($"\nSorted output: {String.Concat (GetSortedList (input, splLetter, orderType))}");
    }
 
-   // This method is used to sort array of letters in ascending or descending order and swap special letters to the last.
-   static string GetSortedArray (string words, string splChar, string order) {
-      List<string> result = [];
-      int count = 0;
-      for (int i = 0; i < words.Length; i++) {
-         if (words[i].ToString () != splChar)
-            result.Add (words[i].ToString ());
-         else count++;
-      }
-      if (order == "d") {
-         result.Sort ();
-         result.Reverse ();
-      } else result.Sort ();
-      result.AddRange (Enumerable.Repeat (splChar, count));
-      return string.Join (",", result);
+   // This method is used to sort list of letters in ascending or descending order and swap special letters to the last.
+   static List<char> GetSortedList (string letters, char splLetter, char orderType) {
+      var result = letters.Where (ch => ch != splLetter).ToList ();
+      result.Sort ();
+      if (orderType == 'D') result.Reverse ();
+      int splLetterCount = letters.Length - result.Count;
+      for (int i = 0; i < splLetterCount; i++) result.Add (splLetter);
+      return result;
    }
    #endregion
 }
