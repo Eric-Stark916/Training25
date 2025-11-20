3640dfb0a5386af6c414524fb4cca875b811b013
Eric <Eric.Sureshkumar@trumpf.com>
2025-11-20 08:29:29 +0530

    R01

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -17,12 +17,14 @@ internal class Program {
          var inp = ReadLine ()?.Trim ().ToLower ();
          if (inp == "x") break;
          else if (int.TryParse (inp, out int nthPlace) && nthPlace is > 0 and < 26)
+            // Upto 10th Armstrong number 'GetArmstrongNum' method is not called.
             WriteLine ($"The #{nthPlace} Armstrong number is: {(nthPlace <= 10 ? nthPlace - 1 : GetArmstrongNum (nthPlace))}\n");
       }
    }
 
    // Returns the nth Armstrong number.
    static string GetArmstrongNum (int nthPlace) {
+      // We start from 153 (11th Armstrong number) to avoid recalculating previous values.
       int num = 153, placeCount = 10;
       while (true) {
          if (IsArmstrongNum (num)) placeCount++;
@@ -35,7 +37,7 @@ internal class Program {
    // Finds whether a number is an Armstrong number or not.
    static bool IsArmstrongNum (int inp) {
       int len = inp.ToString ().Length, sum = 0;
-      for (int temp = inp; temp > 0; temp = temp / 10)
+      for (int temp = inp; temp > 0; temp /= 10)
          sum += (int)Math.Pow (temp % 10, len);
       return sum == inp;
    }
